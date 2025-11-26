b923558e1d2513d44827423efa2c7a89d5f0f2b1
Alisan <144158983+AZCPCF@users.noreply.github.com>
2025-11-27 03:14:38 +0330

    fix: delete note issue

--- src/actions/delete-note-action.ts
+++ src/actions/delete-note-action.ts
@@ -1,9 +1,10 @@
 "use server";
 
+import { fetcher } from "@/lib/fetcher";
 import { revalidatePath } from "next/cache";
 
 export const deleteNoteAction = async (id: string) => {
-  const res = await fetch("/api/notes/" + id, {
+  const res = await fetcher(`/api/notes/${id}`, {
     method: "DELETE",
   });
   if (res.ok && res.status == 204) {
--- src/components/pages/notes/note-card.tsx
+++ src/components/pages/notes/note-card.tsx
@@ -1,31 +1,30 @@
 "use client";
 import { deleteNoteAction } from "@/actions/delete-note-action";
 import { Note } from "@/app/api/notes/db";
+import { useState } from "react";
 import { FaTrash } from "react-icons/fa6";
 
-export default function NoteCard({
-  note,
-  category,
-}: {
-  note: Note;
-  category?: string;
-}) {
+export default function NoteCard({ note, category }: { note: Note; category?: string }) {
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div
-      key={note.id}
-      className="bg-stone-800 p-3 rounded-md flex flex-col gap-1.5 min-h-[158px] relative"
-    >
+    <div className="bg-stone-800 p-3 rounded-md flex flex-col gap-1.5 min-h-[158px] relative">
       <p className="font-extrabold text-xl">{note.title}</p>
-      {category ? <p>category: {category}</p> : ""}
+      {category && <p>category: {category}</p>}
       <p className="truncate">{note.content}</p>
+
       <div className="w-[96%] left-1/2 -translate-x-1/2 flex justify-end absolute bottom-2.5">
         <button
           onClick={async () => {
+            setLoading(true);
             await deleteNoteAction(note.id);
+            setLoading(false);
           }}
-          className="bg-rose-500 rounded-md px-3 py-2 flex items-center md:w-auto w-full justify-center gap-2"
+          data-loading={loading}
+          className="bg-rose-500 data-[loading=true]:bg-rose-400 rounded-md px-3 py-2 flex items-center justify-center gap-2 w-full md:w-auto"
         >
-          <FaTrash /> Delete
+          <FaTrash />
+          {loading ? "Deleting..." : "Delete"}
         </button>
       </div>
     </div>
